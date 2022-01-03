e8aba9052c52f1c17c612bcaac51e9ecb96c5016
Ludovico Russo <ludus.russo@gmail.com>
2022-01-03 15:47:43 +0100

    disable swc

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { withSuperjson } = require("next-superjson");
 
 module.exports = withSuperjson()({
+  swcMinify: false,
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
