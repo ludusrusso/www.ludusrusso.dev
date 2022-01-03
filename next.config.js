7aa6e445384ce77bf2ab9fde2812209aa261d297
Ludovico Russo <ludus.russo@gmail.com>
2022-01-03 15:45:16 +0100

    update package json for vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const { withSuperjson } = require("next-superjson");
 
 module.exports = withSuperjson()({
-  swcMinify: false,
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
