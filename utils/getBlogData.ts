8618d004517a3b7972e6a33ffae255ba44da3c48
Ludovico Russo <ludus.russo@gmail.com>
2022-01-04 11:19:54 +0100

    update

--- utils/getBlogData.ts
+++ utils/getBlogData.ts
@@ -67,7 +67,7 @@ export const getBlogData = async () => {
 };
 
 const _getBlogData = async () => {
-  const files = (await getFiles()).filter((_, idx) => idx < 20);
+  const files = await getFiles();
   return Promise.all(
     files.map(async (file) => {
       const md = await fs.readFile(file);
