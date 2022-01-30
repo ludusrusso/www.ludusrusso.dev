eaa502a62b85cc35da8e12afd18613944d7eb323
Ludovico Russo <ludus.russo@gmail.com>
2022-01-30 22:31:32 +0100

    add robots txt

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   siteUrl: process.env.SITE_URL || "https://www.ludusrusso.dev",
+  generateRobotsTxt: true, // (optional)
   exclude: ["/admin/*", "/episodes/*"],
 };
--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const { withSuperjson } = require("next-superjson");
 
 module.exports = withSuperjson()({
   swcMinify: false,
-  generateRobotsTxt: true, // (optional)
   images: {
     domains: ["avatars.githubusercontent.com", "res.cloudinary.com"],
   },
