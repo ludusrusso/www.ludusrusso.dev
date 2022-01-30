76df847390df08e53feafbcd8906217671fb0607
Ludovico Russo <ludus.russo@gmail.com>
2022-01-30 22:16:19 +0100

    add next sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+  reactStrictMode: true,
+  siteUrl: process.env.SITE_URL || "https://dev.ludusrusso.space",
+  exclude: ["/admin/*", "/episodes/*"],
+};
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const { withSuperjson } = require('next-superjson');
+const { withSuperjson } = require("next-superjson");
 
 module.exports = withSuperjson()({
   swcMinify: false,
-  reactStrictMode: true,
+  generateRobotsTxt: true, // (optional)
   images: {
-    domains: ['avatars.githubusercontent.com', 'res.cloudinary.com'],
+    domains: ["avatars.githubusercontent.com", "res.cloudinary.com"],
   },
 });
