0781a86d1ac81fcdca525da5bd17cff1ad4f87e2
Ludovico Russo <ludus.russo@gmail.com>
2022-04-25 14:33:49 +0200

    remove apollo and update libs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,14 @@
-import { ApolloProvider } from "@apollo/client";
 import { withTRPC } from "@trpc/next";
+import { AuthProvider } from "admin/auth/auth.provider";
+import { authCli } from "admin/services/auth.service";
+import { AppType } from "next/dist/shared/lib/utils";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { AppRouter } from "trpc/server";
 import { SEO } from "../components/seo";
 import "../styles/globals.css";
-import { apolloClient } from "../utils/apollo";
 import * as ga from "../utils/ga";
-import { AppType } from "next/dist/shared/lib/utils";
-import { authCli } from "admin/services/auth.service";
-import { AppRouter } from "trpc/server";
-import { AuthProvider } from "admin/auth/auth.provider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const router = useRouter();
@@ -31,92 +29,90 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
   return (
     <AuthProvider>
-      <ApolloProvider client={apolloClient}>
-        <Head>
-          <meta
-            name="description"
-            content="Ludusrusso è il sito di Ludovico Russo"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="57x57"
-            href="/icon/apple-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="60x60"
-            href="/icon/apple-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/icon/apple-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/icon/apple-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="114x114"
-            href="/icon/apple-icon-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="120x120"
-            href="/icon/apple-icon-120x120.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="144x144"
-            href="/icon/apple-icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/icon/apple-icon-152x152.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icon/apple-icon-180x180.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="/icon/android-icon-192x192.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/icon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="/icon/favicon-96x96.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/icon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/icon/manifest.json" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta
-            name="msapplication-TileImage"
-            content="/icon/ms-icon-144x144.png"
-          />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
-        <SEO />
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <Head>
+        <meta
+          name="description"
+          content="Ludusrusso è il sito di Ludovico Russo"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/icon/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/icon/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/icon/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/icon/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/icon/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/icon/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/icon/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icon/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icon/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/icon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/icon/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-TileImage"
+          content="/icon/ms-icon-144x144.png"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <SEO />
+      <Component {...pageProps} />
     </AuthProvider>
   );
 };
