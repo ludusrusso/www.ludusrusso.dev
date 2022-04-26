96ba65b43226c6757e671beb34ab5311ff96e628
Ludovico Russo <ludus.russo@gmail.com>
2022-04-26 09:49:11 +0200

    fix: banner

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,33 +1,49 @@
 import { BookOpenIcon, XIcon } from "@heroicons/react/outline";
 import Link from "next/link";
+import { ComponentProps, FC } from "react";
 
 export const Banner = () => {
   return (
-    <div className="bg-green-600">
-      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between flex-wrap">
-          <div className="w-0 flex-1 flex items-center">
-            <span className="flex p-2 rounded-lg bg-green-800">
-              <BookOpenIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </span>
-            <p className="ml-3 font-medium text-white truncate">
-              <span className="md:hidden">
-                Nuova sezione libri disponibile!
+    <BannerWithProps
+      Icon={BookOpenIcon}
+      btn="Visita"
+      href="/books"
+      long="Novità! Ecco la nuova sezione dei libri consigliati."
+      short="Nuova sezione libri disponibile!"
+    />
+  );
+};
+
+interface BannterProps {
+  long: string;
+  short: string;
+  btn: string;
+  Icon: FC<ComponentProps<"svg">>;
+  href: string;
+}
+
+const BannerWithProps = ({ long, btn, short, Icon, href }: BannterProps) => {
+  return (
+    <div className="">
+      <div className="">
+        <div className="bg-teal-600 p-2 shadow-lg sm:p-3">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex w-0 flex-1 items-center">
+              <span className="flex rounded-lg bg-teal-800 p-2">
+                <Icon className="h-6 w-6 text-white" aria-hidden="true" />
               </span>
-              <span className="hidden md:inline">
-                Novità! Ecco la nuova sezione dei libri consigliati.
-              </span>
-            </p>
-          </div>
-          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-            <Link href="/books">
-              <a
-                href="#"
-                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-green-600 bg-white hover:bg-indigo-50"
-              >
-                Visita
-              </a>
-            </Link>
+              <p className="ml-3 truncate font-medium text-white">
+                <span className="md:hidden">{short}</span>
+                <span className="hidden md:inline">{long}</span>
+              </p>
+            </div>
+            <div className="mt-0  w-auto flex-shrink-0">
+              <Link href={href}>
+                <a className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-teal-600 shadow-sm hover:bg-cyan-50">
+                  {btn}
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
