5db9448fc9e774c4f042aa19bba997ff57ee6e26
Ludovico Russo <ludus.russo@gmail.com>
2022-06-03 15:06:28 +0200

    fix quote

--- pages/index.tsx
+++ pages/index.tsx
@@ -277,13 +277,13 @@ const HeroSection = () => {
 
 const Quote = () => {
   return (
-    <div className="bg-white pt-16 lg:py-24">
+    <div className="bg-gray-50 pt-16 lg:py-24 mb-10">
       <div className="pb-16 bg-green-600 lg:pb-0 lg:z-10 lg:relative">
         <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
           <div className="relative lg:-my-8">
             <div
               aria-hidden="true"
-              className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
+              className="absolute inset-x-0 top-0 h-1/2 bg-gray-50 lg:hidden"
             />
             <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
               <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
